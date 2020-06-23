fc077ccb0e9aa613b9c7c5bbc10c908fde8af066
“Dennis.r.ysbm” <android6636@gmail.com>
2020-06-24 02:23:26 +0300

    join?

--- app_blog/services/article_service.py
+++ app_blog/services/article_service.py
@@ -4,6 +4,7 @@ from slugify import slugify
 
 from app_blog.extensions import db
 from app_blog.models import Article
+from app_blog.models.category import Category
 
 
 class ArticleService:
@@ -11,8 +12,11 @@ class ArticleService:
         self.articles: List[Article] = []
         self.article: Optional[Article] = None
 
+    # не работает вроде
     def get_articles(self):
-        return Article.query.all()
+        return Article.query\
+            .join(Category, Article.category_id == Category.id)\
+            .all()
 
     def create_article(self, data) -> Article:
         data["slug"] = slugify(data["title"])
